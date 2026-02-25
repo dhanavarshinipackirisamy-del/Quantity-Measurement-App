29ff7426b30a1627d23ab8b8023ae3d054f405b5
Dhanavarshini <dp9387@srmist.edu.in>
2026-02-25 13:06:16 +0530

    [dhana]: UC3: Refactor to Generic Length class applying DRY principle

--- src/main/java/com/bridgelabz/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -52,4 +52,54 @@ public class QuantityMeasurementApp {
             return Double.compare(this.value, other.value) == 0;
         }
     }
+    public static class Length {
+
+        private final double value;
+        private final LengthUnit unit;
+
+        public enum LengthUnit {
+            FEET(12.0),      // 1 foot = 12 inches
+            INCHES(1.0);
+
+            private final double conversionFactor;
+
+            LengthUnit(double conversionFactor) {
+                this.conversionFactor = conversionFactor;
+            }
+
+            public double getConversionFactor() {
+                return conversionFactor;
+            }
+        }
+
+        public Length(double value, LengthUnit unit) {
+            if (unit == null)
+                throw new IllegalArgumentException("Unit cannot be null");
+
+            this.value = value;
+            this.unit = unit;
+        }
+
+        private double toBaseUnit() {
+            return value * unit.getConversionFactor();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj)
+                return true;
+
+            if (obj == null)
+                return false;
+
+            if (getClass() != obj.getClass())
+                return false;
+
+            Length other = (Length) obj;
+
+            return Double.compare(this.toBaseUnit(),
+                    other.toBaseUnit()) == 0;
+        }
+    }
 }
